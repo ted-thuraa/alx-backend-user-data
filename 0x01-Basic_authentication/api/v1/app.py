46289b974ed252811f2314933b32f1a3a9ccaa62
ted-thuraa <mutwirited@gmail.com>
2023-02-07 09:18:42 +0300

    corr

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,48 +1,51 @@
 #!/usr/bin/env python3
+"""Route module for the API.
 """
-Route module for the API
-"""
+import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-from api.v1.auth import Auth
+
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
     auth = BasicAuth()
 
-@app.errorhandler(404)
-def not_authorised(error) -> str:
-    '''Unauthorised handler
-    '''
-    return jsonify({"error": "Unauthorized"}), 401
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """Not found handler.
     """
     return jsonify({"error": "Not found"}), 404
 
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized handler.
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden handler
+    """Forbidden handler.
     """
     return jsonify({"error": "Forbidden"}), 403
 
+
 @app.before_request
 def authenticate_user():
-    """Authenticate user before processing a request
+    """Authenticates a user before processing a request.
     """
     if auth:
         excluded_paths = [
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,9 +11,11 @@ from models.user import User
 
 
 class BasicAuth(Auth):
-    """Basic authentication class
+    """Basic authentication class.
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
         """Extracts the Base64 part of the Authorization header
         for a Basic Authentication.
         """
@@ -24,8 +26,11 @@ class BasicAuth(Auth):
                 return field_match.group('token')
         return None
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
-        """ returns the decoded value of a Base64 string base64_authorization_header
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str,
+            ) -> str:
+        """Decodes a base64-encoded authorization header.
         """
         if type(base64_authorization_header) == str:
             try:
@@ -37,7 +42,10 @@ class BasicAuth(Auth):
             except (binascii.Error, UnicodeDecodeError):
                 return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str,
+            ) -> Tuple[str, str]:
         """Extracts user credentials from a base64-decoded authorization
         header that uses the Basic authentication flow.
         """
@@ -53,9 +61,11 @@ class BasicAuth(Auth):
                 return user, password
         return None, None
 
-
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
-        """ returns the User instance based on his email and password.
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """Retrieves a user based on the user's authentication credentials.
         """
         if type(user_email) == str and type(user_pwd) == str:
             try:
